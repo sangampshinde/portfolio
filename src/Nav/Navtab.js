825dc0bb4a76a422c989d358a7d3f395f1093324
sangam shinde <sangampshinde@gmail.com>
2024-05-22 12:29:25 +0530

    adding global css

--- src/Nav/Navtab.js
+++ src/Nav/Navtab.js
@@ -1,13 +1,26 @@
-import React from 'react'
+import React,{useRef} from 'react'
 import './Navtab.css'
 
 function Navtab({tabs,onChange}) {
 
+
+
+    const elements = useRef(new Array())
     console.log('tabs:',tabs);
   return (
-    <div>
-      <h2>hello from Navtab</h2>
-    </div>
+    <nav className='nav__tabs'>
+        {
+            tabs.map((name,index)=>(
+                <button
+                key={index}
+                ref={el=>elements.current[index]=el}
+                onClick={name}
+                
+                ></button>
+            ))
+        }
+     
+    </nav>
   )
 }
 
