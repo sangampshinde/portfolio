7f1b87df4e7d3024ec687f843907ba11ec416105
sangam shinde <sangampshinde@gmail.com>
2024-05-22 12:03:30 +0530

    project setup

---
+++ src/Nav/Navtab.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Navtab.css'
+
+function Navtab({tabs,onChange}) {
+
+    console.log('tabs:',tabs);
+  return (
+    <div>
+      <h2>hello from Navtab</h2>
+    </div>
+  )
+}
+
+export default Navtab
---
+++ src/data.js
@@ -0,0 +1,75 @@
+export const data=
+[
+
+{
+    id:1,
+    group:'Profile',
+    image:'',
+    title:'',
+    github:'',
+    demo:''
+},
+
+{
+    id:2,
+    group:'ReactJs',
+    image:'',
+    title:'',
+    github:'',
+    demo:''
+},
+
+{
+    id:3,
+    group:'NodeJs',
+    image:'',
+    title:'',
+    github:'',
+    demo:''
+},
+
+{
+    id:4,
+    group:'Angular',
+    image:'',
+    title:'',
+    github:'',
+    demo:''
+},
+
+{
+    id:5,
+    group:'Figma',
+    image:'',
+    title:'',
+    github:'',
+    demo:''
+},
+
+{
+    id:6,
+    group:'Wordpress',
+    image:'',
+    title:'',
+    github:'',
+    demo:''
+},
+
+{
+    id:7,
+    group:'JavaScript',
+    image:'',
+    title:'',
+    github:'',
+    demo:''
+},
+
+{
+    id:8,
+    group:'reactNative',
+    image:'',
+    title:'',
+    github:'',
+    demo:''
+},
+]
